22d2d607cb2460ebd1fa30cc8ba12359bddb39ba
Anton Veldanov <anton.veldanov@gmail.com>
2019-07-19 21:03:41 -0700

    prod

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 
-const PORT = process.env.PORT || 4100;
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`, this.address().port, app.settings.env));
 
 
