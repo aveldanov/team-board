29c840022eaa0ec8f16fe66c94ce352ba59ee278
Anton Veldanov <anton.veldanov@gmail.com>
2019-07-19 20:02:26 -0700

    prod

--- server.js
+++ server.js
@@ -26,8 +26,9 @@ app.use('/api/tasks', require('./routes/tasks'));
 // Serve Static Assets in production
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
 
+
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 
@@ -35,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4100;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
